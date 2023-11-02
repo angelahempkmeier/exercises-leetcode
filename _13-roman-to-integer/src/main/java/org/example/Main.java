390837bd3ae0bf81b2462084a1c6e961613c0957
Angela Hempkmeier <aggsdpz@gmail.com>
2023-11-01 21:04:39 -0300

    continuing roman

--- _13-roman-to-integer/src/main/java/org/example/Main.java
+++ _13-roman-to-integer/src/main/java/org/example/Main.java
@@ -18,11 +18,33 @@ public class Main {
         return value;
     }
 
+    private static int returnIntValue(String romanValue){
+        int i;
+        switch (romanValue){
+            case "I": return 1;
+            case "V": return 5;
+            case "X": return 10;
+            case "L": return 50;
+            case "C": return 100;
+            case "D": return 500;
+            case "M": return 1000;
+            default:
+                i = 0;
+        }
+        return i;
+    }
+
     private static int algToInt(String first, String second){
         int value = 0;
 
         if(first.equals("I") && second.equals("V")){
             value = 4;
+        }else if(first.equals("I") && second.equals("X")){
+            value = 9;
+        } else if (first.equals("X") && second.equals("L")) {
+            value = 40;
+        } else if (first.equals("X") && second.equals("C")) {
+            value = 90;
         }
         return value;
     }
